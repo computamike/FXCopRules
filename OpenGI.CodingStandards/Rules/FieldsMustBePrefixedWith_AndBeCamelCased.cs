476ce34e6f8af0d4db2e6d321dbf05d15109e7b9
= <mhingley@OGW-PC13442>
2017-02-22 17:25:43 +0000

    Field name checking... Property name Checking

---
+++ OpenGI.CodingStandards/Rules/FieldsMustBePrefixedWith_AndBeCamelCased.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.FxCop.Sdk;
+
+
+namespace OpenGI.CodingStandards.Rules
+{
+    public class FieldsMustBePrefixedWith_AndBeCamelCased : BaseRule
+    {
+        public FieldsMustBePrefixedWith_AndBeCamelCased() :
+            base("FieldsMustBePrefixedWith_AndBeCamelCased")
+        {
+            // System.Diagnostics.Debugger.Launch();
+        }
+
+        // inspiration : http://www.binarycoder.net/fxcop/html/ex_classfieldnameprefixes.html
+        public override ProblemCollection Check(Member member)
+        {
+            if (!(member.DeclaringType is ClassNode))
+                return this.Problems;
+
+            Field fld = member as Field;
+            if (fld == null)
+                return this.Problems;
+
+            if (!fld.IsStatic &&  !fld.Name.Name.StartsWith("_", StringComparison.Ordinal) && !fld.IsCompilerControlled )
+            {
+                this.Problems.Add(new Problem(this.GetNamedResolution("Instance", fld.Name.Name)));
+            }
+
+            return this.Problems;
+        }
+
+
+    }
+}
---
+++ OpenGI.CodingStandards/Rules/PropertyNamesShouldbePascalCased.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.FxCop.Sdk;
+
+namespace OpenGI.CodingStandards.Rules
+{
+    public class PropertyNamesShouldbePascalCased : BaseRule
+    {
+        public PropertyNamesShouldbePascalCased():
+            base("PropertyNamesShouldbePascalCased")
+        {
+                       // System.Diagnostics.Debugger.Launch();
+        }
+
+        public override ProblemCollection Check(Member member)
+        {
+            PropertyNode meth = member as PropertyNode;
+            if (meth != null &&  meth.NodeType != NodeType.InstanceInitializer)
+            {
+                string pascalMethodName = meth.Name.Name.ToPascalstring();
+                if (!meth.Name.Name.Equals(pascalMethodName))
+                { 
+                    this.Problems.Add(new Problem(this.GetResolution(), meth.SourceContext ));
+                }
+            }
+            return Problems;
+        }
+
+
+
+
+
+    }
+}
